892ea828e94b4a6a6ab5a043681bc309377ae136
Pablo <pablo.diaz@mendix.com>
2022-06-08 07:40:20 +0200

    New Commit

---
+++ deployment/modern-web/start.bat
@@ -0,0 +1,1 @@
+"C:/Program Files/Mendix/9.6.5.34071\modeler\tools\node\node.exe" "C:/Program Files/Mendix/9.6.5.34071\modeler\tools\node\node_modules\rollup\dist\bin\rollup" -c -w
---
+++ deployment/native/styles.js
@@ -0,0 +1,11 @@
+import * as style0 from "//Mac/Home/Documents/Mendix/Pablo - LNowApp-main/themesource/administration/native/main";
+import * as style1 from "//Mac/Home/Documents/Mendix/Pablo - LNowApp-main/themesource/anychart_buildingblocks/native/main";
+import * as style2 from "//Mac/Home/Documents/Mendix/Pablo - LNowApp-main/themesource/nanoflowcommons/native/main";
+import * as style3 from "//Mac/Home/Documents/Mendix/Pablo - LNowApp-main/themesource/webactions/native/main";
+import * as style4 from "//Mac/Home/Documents/Mendix/Pablo - LNowApp-main/themesource/atlas_core/native/main";
+import * as style5 from "//Mac/Home/Documents/Mendix/Pablo - LNowApp-main/themesource/myfirstmodule/native/main";
+import * as style6 from "//Mac/Home/Documents/Mendix/Pablo - LNowApp-main/theme/native/main";
+
+import { flatten } from "mendix/native";
+
+module.exports = flatten([style0, style1, style2, style3, style4, style5, style6]);
